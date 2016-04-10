7bf7678ff3a41a3fbe52cf5e4273c9fd2d1242f0
maryam <maryam@PGs-MacBook-Pro-2.local>
2016-04-10 15:51:13 +0430

    Add hot reload

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.get('/build/app.js', function(req, res) {
+//   res.sendFile('app.js', {
+//      root: __dirname + '/build/',
+//   })
+// });
+//
+// app.get('*', function(req, res) {
+//   res.sendFile('index.html', {
+//      root: __dirname + '/src/',
+//   })
+// });
+// app.listen(3000);
 
-app.get('/build/app.js', function(req, res) {
-  res.sendFile('app.js', {
-     root: __dirname + '/build/',
-  })
-});
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
-app.get('*', function(req, res) {
-  res.sendFile('index.html', {
-     root: __dirname + '/src/',
-  })
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  contentBase: 'build/',
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at http://localhost:3000/');
 });
-app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    "./src/index.js"
+  ],
   output: {
     path: __dirname + "/build",
-    filename: "app.js"
+    filename: "app.js",
+    publicPath: '/kooft/'
   },
   module: {
     loaders: [
@@ -12,10 +19,20 @@ module.exports = {
 
         // "exclude" should be used to exclude exceptions
         // try to prefer "include" when possible
-
-        // the "loader"
-        loader: "babel"
+        exclude: /node_modules/,
+        // the "loader"      
+        loaders: ['react-hot', "babel"]
+      },
+      {
+          test: /\.css$/,
+          loaders: [
+              'style?sourceMap',
+              'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+          ]
       }
     ]
-  }
+  },
+  plugins: [
+       new webpack.HotModuleReplacementPlugin()
+   ]
 }
